993789f1dc577c5c207c90cd1c70dffbacd17349
Diego Olvera <diego.olvera97@gmail.com>
2022-01-02 00:22:20 -0600

    Created url component (main component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UrlComponent } from './url/url.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlComponent
   ],
   imports: [
     BrowserModule
