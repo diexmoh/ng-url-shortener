9ff803f31965b30d9a5577f5e450ddefed63d42f
Diego Olvera <diego.olvera97@gmail.com>
2022-01-15 23:27:12 -0600

    added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UrlComponent } from './url/url.component';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UrlComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
